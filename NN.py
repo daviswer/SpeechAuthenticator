10545dd229fba61ada4dbb7cb4d5938c8e1a0980
daviswert <daviswer@stanford.edu>
2015-11-12 21:40:32 -0800

    Code, day 2
    Finished MFCC, NTN, and maybe SNN. Softmax results a little wonky, will
    continue playing.

---
+++ NN.py
@@ -0,0 +1,71 @@
+import numpy as np
+import random
+from copy import deepcopy
+from random import shuffle
+from numpy import dot, outer
+
+class ShallowNeuralNetwork:
+    def __init__(self, input_dim=729, hidden_dim=27, output_dim=1, afunc=np.tanh, d_afunc=(lambda z : 1.0 - z**2)):        
+        self.afunc = afunc 
+        self.d_afunc = d_afunc      
+        self.input = np.ones(input_dim)                                         
+        self.hidden = np.ones(hidden_dim+1)      
+        self.output = np.ones(output_dim)        
+        self.iweights = np.random.normal(scale=0.0001, size=(input_dim, hidden_dim))
+        self.oweights = np.random.normal(scale=0.0001, size=(hidden_dim+1, output_dim)) 
+        self.ierr = np.zeros(self.iweights.shape)
+        self.oerr = np.zeros(self.oweights.shape)
+#         self.imom = np.zeros(self.iweights.shape)
+#         self.omom = np.zeros(self.oweights.shape)
+        
+    def forward_propagation(self, ex):
+        self.input = ex
+        self.hidden[:-1] = self.afunc(dot(self.input, self.iweights))
+        self.output = self.afunc(dot(self.hidden, self.oweights))
+        
+    def backward_propagation(self, labels, alpha=0.5):
+        labels = np.array(labels)       
+        oerr = (labels-self.output) * self.d_afunc(self.output)
+        herr = dot(oerr, self.oweights.T) * self.d_afunc(self.hidden)
+        self.oweights += alpha * outer(self.hidden, oerr)
+        self.iweights += alpha * outer(self.input, herr[:-1])
+        return np.sum(0.5 * (labels-self.output)**2)
+
+    def train(self, training_data, maxiter=5000, alpha=0.05, lmbda=0, epsilon=1.5e-8, display_progress=False):       
+        iteration = 0
+        error = sys.float_info.max
+        while error > epsilon and iteration < maxiter:
+#             gamma = 1/(2+math.trunc(np.sqrt(iteration)))
+            error = 0.0
+            size = 0.0
+            shuffle(training_data)
+            for ex, labels in training_data:
+                self.forward_propagation(ex)
+                size += abs(self.output)
+                error += self.backward_propagation(labels, alpha=alpha)
+#                 self.imom = self.ierr + gamma*self.imom
+#                 self.omom = self.oerr + gamma*self.omom
+#                 self.iweights += self.imom #- lmbda*self.l2penalty(self.iweights)
+#                 self.oweights += self.omom #- lmbda*self.l2penalty(self.oweights)
+#                 self.iweights = self.iweights*(1-lmbda)
+#                 self.oweights = self.oweights*(1-lmbda)
+            if display_progress and iteration%10==0:
+                print 'completed iteration %s; error is %s; size is %s' % (iteration, error, np.sum(np.absolute(self.iweights)))
+            iteration += 1
+            
+    def accuracy(self, data):
+        score = 0.0
+        for ex, label in data:
+            if sign(self.predict(ex)[0])==label: score+=1
+        return score/len(data)
+            
+    def l2penalty(self, arr):
+        return np.multiply(np.array(map(lambda x: linalg.norm(arr[:,x]), range(len(arr[0])))), arr)
+                    
+    def predict(self, ex):
+        self.forward_propagation(ex)
+        return deepcopy(self.output)
+        
+    def hidden_representation(self, ex):
+        self.forward_propagation(ex)
+        return self.hidden
---
+++ WAVreader.py
@@ -0,0 +1,108 @@
+import numpy as np
+from scipy.io import wavfile
+from scipy.fftpack import fft, dct
+from pylab import *
+from numpy import dot
+
+def getData(filename, label):
+    
+    def plots(i):
+        print "WAV DATA, FFT, MEL CEPSTRALS, SCALED CEPSTRALS, DELTAS, SCALED DELTAS"
+        figure(0)
+        plot(samples[i])
+        figure(1)
+        plot(melLabel, ffts[i])
+        figure(2)
+        plot(MFCCs[i])
+        figure(3)
+        plot(lnMFCCs[i])
+        figure(4)
+        plot(deltas[i-2])
+        figure(5)
+        plot(lnDeltas[i-2])
+        show()
+    
+    print
+    print "Reading file "+filename
+    rate, x = wavfile.read(filename);
+    samplelen = rate*0.03
+    samples = [x[i*samplelen/2:i*samplelen/2+samplelen]/100.0 for i in range(int(len(x)/samplelen*2))]
+    print "Sample split into %d windows of length %d" % (len(samples), samplelen)
+    ffts = [c[:samplelen/2-1] for c in map(lambda i: abs(fft(i)), samples)]
+    T = samplelen/rate
+    frqLabel = map(lambda i: i/T, range(int(samplelen/2-1)))
+    print "Frequencies range from %f to %f" % (frqLabel[0], frqLabel[-1])
+    melLabel = map(lambda i: 1125*log(1+i/700), frqLabel)
+    print "Mel frequencies range from %f to %f" % (melLabel[0], melLabel[-1])
+    melPoints = [(melLabel[-1]-1.0)/27*i for i in range(28)]
+    print
+    print "Building filters..."
+    melFilters = [[max(0, min(1.0/(melPoints[i+1]-melPoints[i])*(x-melPoints[i+1]+.0)+1.0,
+                            -1.0/(melPoints[i+2]-melPoints[i+1])*(x-melPoints[i+1]+.0)+1.0))
+                 for x in melLabel] for i in range(26)]
+    print "Applying filters..."
+    Mfs = [[np.dot(f, melFilters[i]) for i in range(26)] for f in ffts]
+    print "Applying cosine transform..."
+    MFCCs = [dct(Mf) for Mf in Mfs]
+    print "MFCC complete!"
+    print
+    print "Calculating deltas..."
+    deltas = [[(MFCCs[i+1][j]+2*MFCCs[i+2][j]-MFCCs[i-1][j]-2*MFCCs[i-2][j])/10 
+              for j in range(26)] for i in range(2,len(MFCCs)-2)]
+    print "Deltas complete!"
+    print
+    print "Scaling..."
+    lnMFCCs = [[np.sign(c[i])*log(abs(c[i])/1000) for i in range(26)] for c in MFCCs]
+    lnDeltas = [[np.sign(c[i])*log(abs(c[i])/10) for i in range(26)] for c in deltas]
+    print "Scaling complete!"
+    print
+    print "Merging..."
+    result = [(np.reshape(np.outer(np.append(lnMFCCs[i+2], [1]),np.append(lnDeltas[i], [1])), 729),
+               label) for i in range(len(lnDeltas))]
+#     result = [(mfcc, label) for mfcc in lnMFCCs]
+    print "Finished merging!"
+    return result
+
+
+def getMulticlassData(filename, label, numcat):
+    
+    print
+    print "Reading file "+filename
+    rate, x = wavfile.read(filename);
+    samplelen = rate*0.03
+    samples = [x[i*samplelen/2:i*samplelen/2+samplelen]/100.0 for i in range(int(len(x)/samplelen*2))]
+    print "Sample split into %d windows of length %d" % (len(samples), samplelen)
+    ffts = [c[:samplelen/2-1] for c in map(lambda i: abs(fft(i)), samples)]
+    T = samplelen/rate
+    frqLabel = map(lambda i: i/T, range(int(samplelen/2-1)))
+    print "Frequencies range from %f to %f" % (frqLabel[0], frqLabel[-1])
+    melLabel = map(lambda i: 1125*log(1+i/700), frqLabel)
+    print "Mel frequencies range from %f to %f" % (melLabel[0], melLabel[-1])
+    melPoints = [(melLabel[-1]-1.0)/27*i for i in range(28)]
+    print
+    print "Building filters..."
+    melFilters = [[max(0, min(1.0/(melPoints[i+1]-melPoints[i])*(x-melPoints[i+1]+.0)+1.0,
+                            -1.0/(melPoints[i+2]-melPoints[i+1])*(x-melPoints[i+1]+.0)+1.0))
+                 for x in melLabel] for i in range(26)]
+    print "Applying filters..."
+    Mfs = [[np.dot(f, melFilters[i]) for i in range(26)] for f in ffts]
+    print "Applying cosine transform..."
+    MFCCs = [dct(Mf) for Mf in Mfs]
+    print "MFCC complete!"
+    print
+    print "Calculating deltas..."
+    deltas = [[(MFCCs[i+1][j]+2*MFCCs[i+2][j]-MFCCs[i-1][j]-2*MFCCs[i-2][j])/10 
+              for j in range(26)] for i in range(2,len(MFCCs)-2)]
+    print "Deltas complete!"
+    print
+    print "Scaling..."
+    lnMFCCs = [[np.sign(c[i])*log(abs(c[i])/1000) for i in range(26)] for c in MFCCs]
+    lnDeltas = [[np.sign(c[i])*log(abs(c[i])/10) for i in range(26)] for c in deltas]
+    print "Scaling complete!"
+    print
+    print "Merging..."
+    result = [(np.reshape(np.outer(np.append(lnMFCCs[i+2], [1]),np.append(lnDeltas[i], [1])), 729),
+               [1 if j==label else 0 for j in range(numcat)]) for i in range(len(lnDeltas))]
+#     result = [(mfcc, label) for mfcc in lnMFCCs]
+    print "Finished merging!"
+    return result
