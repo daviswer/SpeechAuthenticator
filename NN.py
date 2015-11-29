948e6354320ecddb3e08f7d1fad67f7cdf265d91
daviswert <daviswer@stanford.edu>
2015-11-28 19:11:27 -0800

    More tests; added filtering, L2
    Unfortunately, softmax doesn't seem to converge on filtered data for 5
    classes...

--- NN.py
+++ NN.py
@@ -17,8 +17,8 @@ class ShallowNeuralNetwork:
         self.oweights = np.random.normal(scale=0.0001, size=(hidden_dim+1, output_dim)) 
         self.ierr = np.zeros(self.iweights.shape)
         self.oerr = np.zeros(self.oweights.shape)
-#         self.imom = np.zeros(self.iweights.shape)
-#         self.omom = np.zeros(self.oweights.shape)
+        self.imom = np.zeros(self.iweights.shape)
+        self.omom = np.zeros(self.oweights.shape)
         
     def forward_propagation(self, ex):
         self.input = ex
@@ -29,15 +29,15 @@ class ShallowNeuralNetwork:
         labels = np.array(labels)       
         oerr = (labels-self.output) * self.d_afunc(self.output)
         herr = dot(oerr, self.oweights.T) * self.d_afunc(self.hidden)
-        self.oweights += alpha * outer(self.hidden, oerr)
-        self.iweights += alpha * outer(self.input, herr[:-1])
+        self.oerr = alpha * outer(self.hidden, oerr)
+        self.ierr = alpha * outer(self.input, herr[:-1])
         return np.sum(0.5 * (labels-self.output)**2)
 
     def train(self, training_data, maxiter=5000, alpha=0.05, lmbda=0, epsilon=1.5e-8, display_progress=False):       
         iteration = 0
         error = sys.float_info.max
         while error > epsilon and iteration < maxiter:
-#             gamma = 1/(2+math.trunc(np.sqrt(iteration)))
+            gamma = 1.0/(2+iteration)#math.trunc(np.sqrt(iteration)))
             error = 0.0
             size = 0.0
             shuffle(training_data)
@@ -45,12 +45,12 @@ class ShallowNeuralNetwork:
                 self.forward_propagation(ex)
                 size += abs(self.output)
                 error += self.backward_propagation(labels, alpha=alpha)
-#                 self.imom = self.ierr + gamma*self.imom
-#                 self.omom = self.oerr + gamma*self.omom
-#                 self.iweights += self.imom #- lmbda*self.l2penalty(self.iweights)
-#                 self.oweights += self.omom #- lmbda*self.l2penalty(self.oweights)
-#                 self.iweights = self.iweights*(1-lmbda)
-#                 self.oweights = self.oweights*(1-lmbda)
+                self.imom = self.ierr + gamma*self.imom
+                self.omom = self.oerr + gamma*self.omom
+                self.iweights += self.imom - lmbda*self.l2penalty(self.iweights)
+                self.oweights += self.omom - lmbda*self.l2penalty(self.oweights)
+                #self.iweights = self.iweights*(1-lmbda)
+                #self.oweights = self.oweights*(1-lmbda)
             if display_progress and iteration%10==0:
                 print 'completed iteration %s; error is %s; size is %s' % (iteration, error, np.sum(np.absolute(self.iweights)))
             iteration += 1
@@ -58,11 +58,11 @@ class ShallowNeuralNetwork:
     def accuracy(self, data):
         score = 0.0
         for ex, label in data:
-            if sign(self.predict(ex)[0])==label: score+=1
+            if self.predict(ex)[0]/abs(self.predict(ex)[0])==label: score+=1
         return score/len(data)
             
     def l2penalty(self, arr):
-        return np.multiply(np.array(map(lambda x: linalg.norm(arr[:,x]), range(len(arr[0])))), arr)
+        return np.multiply(np.array(map(lambda x: np.linalg.norm(arr[:,x]), range(len(arr[0])))), arr)
                     
     def predict(self, ex):
         self.forward_propagation(ex)
--- WAVreader.py
+++ WAVreader.py
@@ -4,7 +4,14 @@ from scipy.fftpack import fft, dct
 from pylab import *
 from numpy import dot
 
-def getData(filename, label):
+def purge(dat):
+    result = []
+    for window in dat:
+        if np.sum(window) > 2: result.append(window)
+    return result
+
+    
+def getData(filename, label, filter=False):
     
     def plots(i):
         print "WAV DATA, FFT, MEL CEPSTRALS, SCALED CEPSTRALS, DELTAS, SCALED DELTAS"
@@ -28,6 +35,9 @@ def getData(filename, label):
     samplelen = rate*0.03
     samples = [x[i*samplelen/2:i*samplelen/2+samplelen]/100.0 for i in range(int(len(x)/samplelen*2))]
     print "Sample split into %d windows of length %d" % (len(samples), samplelen)
+    if filter==True: 
+        samples = purge(samples)
+        print "Filtered down to %d informative windows" % (len(samples))
     ffts = [c[:samplelen/2-1] for c in map(lambda i: abs(fft(i)), samples)]
     T = samplelen/rate
     frqLabel = map(lambda i: i/T, range(int(samplelen/2-1)))
@@ -44,27 +54,21 @@ def getData(filename, label):
     Mfs = [[np.dot(f, melFilters[i]) for i in range(26)] for f in ffts]
     print "Applying cosine transform..."
     MFCCs = [dct(Mf) for Mf in Mfs]
-    print "MFCC complete!"
-    print
     print "Calculating deltas..."
     deltas = [[(MFCCs[i+1][j]+2*MFCCs[i+2][j]-MFCCs[i-1][j]-2*MFCCs[i-2][j])/10 
               for j in range(26)] for i in range(2,len(MFCCs)-2)]
-    print "Deltas complete!"
-    print
     print "Scaling..."
     lnMFCCs = [[np.sign(c[i])*log(abs(c[i])/1000) for i in range(26)] for c in MFCCs]
     lnDeltas = [[np.sign(c[i])*log(abs(c[i])/10) for i in range(26)] for c in deltas]
-    print "Scaling complete!"
-    print
     print "Merging..."
     result = [(np.reshape(np.outer(np.append(lnMFCCs[i+2], [1]),np.append(lnDeltas[i], [1])), 729),
                label) for i in range(len(lnDeltas))]
 #     result = [(mfcc, label) for mfcc in lnMFCCs]
-    print "Finished merging!"
+    print "Done!"
     return result
 
 
-def getMulticlassData(filename, label, numcat):
+def getMulticlassData(filename, label, numcat, filter=False):
     
     print
     print "Reading file "+filename
@@ -72,6 +76,9 @@ def getMulticlassData(filename, label, numcat):
     samplelen = rate*0.03
     samples = [x[i*samplelen/2:i*samplelen/2+samplelen]/100.0 for i in range(int(len(x)/samplelen*2))]
     print "Sample split into %d windows of length %d" % (len(samples), samplelen)
+    if filter==True: 
+        samples = purge(samples)
+        print "Filtered down to %d informative windows" % (len(samples))
     ffts = [c[:samplelen/2-1] for c in map(lambda i: abs(fft(i)), samples)]
     T = samplelen/rate
     frqLabel = map(lambda i: i/T, range(int(samplelen/2-1)))
@@ -88,21 +95,15 @@ def getMulticlassData(filename, label, numcat):
     Mfs = [[np.dot(f, melFilters[i]) for i in range(26)] for f in ffts]
     print "Applying cosine transform..."
     MFCCs = [dct(Mf) for Mf in Mfs]
-    print "MFCC complete!"
-    print
     print "Calculating deltas..."
     deltas = [[(MFCCs[i+1][j]+2*MFCCs[i+2][j]-MFCCs[i-1][j]-2*MFCCs[i-2][j])/10 
               for j in range(26)] for i in range(2,len(MFCCs)-2)]
-    print "Deltas complete!"
-    print
     print "Scaling..."
     lnMFCCs = [[np.sign(c[i])*log(abs(c[i])/1000) for i in range(26)] for c in MFCCs]
     lnDeltas = [[np.sign(c[i])*log(abs(c[i])/10) for i in range(26)] for c in deltas]
-    print "Scaling complete!"
-    print
     print "Merging..."
     result = [(np.reshape(np.outer(np.append(lnMFCCs[i+2], [1]),np.append(lnDeltas[i], [1])), 729),
                [1 if j==label else 0 for j in range(numcat)]) for i in range(len(lnDeltas))]
 #     result = [(mfcc, label) for mfcc in lnMFCCs]
-    print "Finished merging!"
+    print "Done!"
     return result
